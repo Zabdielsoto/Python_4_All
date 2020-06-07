effaacb8a2f9493ded338fa033bd38bd8e2d59ff
Zabdiel Soto <zabdiel.soto@gmail.com>
2020-06-07 14:40:51 -0500

    Add files via upload

---
+++ Ejercicio2.1.py
@@ -0,0 +1,7 @@
+#Escribe un programa que use input para pedirle al usuario su nombre y luego
+#darle la bienvenida
+
+nombre = input("Escribe tu nombre: ") #desplegamos un mensaje, esperamos a que
+#el usuario ingrese un dato y lo guardamos en la variable nombre
+print("Hola,", nombre) #desplegamos un mensaje junto con el dato de la variable
+#nombre
